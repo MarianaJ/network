5e782152fdc6565d4f60124da35c7f2aecb3c0ab
MarianaJ <emariana@ukr.net>
2017-03-28 01:36:48 +0300

    zadanie 3

--- js/ntw.js
+++ js/ntw.js
@@ -24,4 +24,19 @@ function checkConnection() {
     alert('Connection type: ' + states[networkState]);
 }
 
+
+
 checkConnection();
+
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+        'Acceleration Y: ' + acceleration.y + '\n' +
+        'Acceleration Z: ' + acceleration.z + '\n' +
+        'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+navigator.accelerometer.getCurrentAcceleration(onSuccess, onError);
